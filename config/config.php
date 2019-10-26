bfff7a8fe93fd61586400d26a52d8eb6631a2340
Roman <rumsnownsk@gmail.com>
2019-10-26 13:06:56 +0700

    next

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'settings' => [],
+    'mail' => [
+        'login' => '',
+        'pass' => '',
+        'mailServer' => 'smtp.yandex.ru',
+        'port' => 465,
+        'encryption' => 'SSL',
+    ],
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'pf_nsk',
+        'username' => 'root',
+        'password' => 'root',
+        'charset'=>'utf8',
+        'collation'=>'utf8_unicode_ci',
+        'prefix'    => ''
+    ]
+];

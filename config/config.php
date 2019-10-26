c67c77604ef948a5a955a29a7682eb19cf4f7198
Roman <rumsnownsk@gmail.com>
2019-10-26 13:46:21 +0700

    next1

--- config/config.php
+++ config/config.php
@@ -1,2 +1,27 @@
 <?php
-return [];
+
+return [
+    'components' => [
+        'cache' => 'app\core\libs\Cache',
+//        'test' => 'app\core\libs\Test',
+        'main' => 'app\models\Main'
+    ],
+    'settings' => [],
+    'mail' => [
+        'login' => '',
+        'pass' => '',
+        'mailServer' => 'smtp.yandex.ru',
+        'port' => 465,
+        'encryption' => 'SSL',
+    ],
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'pf_nsk',
+        'username' => 'root',
+        'password' => 'root',
+        'charset'=>'utf8',
+        'collation'=>'utf8_unicode_ci',
+        'prefix'    => ''
+    ]
+];
